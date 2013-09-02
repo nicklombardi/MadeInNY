dff7b7c745805daf01840a9e07f62e8fd4a7c53e
nicklombardi <nicklombardi@gmail.com>
2013-09-01 23:19:42 -0400

    initial

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module MadeInNY
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+     # Enable the asset pipeline
+    config.assets.enabled = true
+
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # config.assets.paths << Rails.root.join("app", "assets", "ico")
