6c1aeb63d5480abd552e6297e0231597ab6bffd1
nicklombardi <nicklombardi@gmail.com>
2013-09-01 22:14:34 -0400

    initial

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'turbolinks'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+  gem 'unicorn'
+  gem 'dalli'
+  gem 'memcachier'
+  gem 'rack-cache'
+  # gem 'rack-google-analytics'
+  gem "rack-timeout"
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MadeInNY::Application.config.secret_key_base = '50ec0dbd1a54d30a9581075f78c86596301040100f766bc1a4e15f5409b4e28454bb7183c27f0fdc82d4315a46babf5aa6dd4357fef9a6ee73942dfeea43d999'
