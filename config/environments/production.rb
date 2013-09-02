d16d3ab3415562d9c329ea547114e030320bcd1c
nicklombardi <nicklombardi@gmail.com>
2013-09-01 23:24:12 -0400

    initial

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ MadeInNY::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
