e275ec8efc881191cfbc36d45a4688482b97b023
Christine Coulter <christinetcoulter@gmail.com>
2013-09-03 13:04:11 -0400

    Make home controller index action respond to json. Add code with ajax request to home controller index action. Successfully retrieve data.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var getCompanyData = function() {
+  console.log("welcome to the uss homestar.com");
+
+  // retrieves data from home controller index action
+  $.ajax({
+    url: '/home.json',
+    dataType: 'json',
+    data: 'GET'
+  }).done(function (data) {
+    console.log(data);
+  });
+
+};
+
+$(document).ready(function() {
+  console.log("your mom");
+  getCompanyData();
+});
