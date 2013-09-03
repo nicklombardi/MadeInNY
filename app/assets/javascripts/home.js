7893c61767928ab23d10585fbd6d435a39d759a6
Christine Coulter <christinetcoulter@gmail.com>
2013-09-03 14:55:36 -0400

    Populate global variable dataset with correct formatting for funding in home.js file.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -22,11 +22,30 @@ var getCompanyData = {
     prepDataForD3: function(data) {
         console.log("prepping data");
         var i,
-            length = data.length;
+            length = data.length,
+            fundingString,
+            fundingArray,
+            thousandOrMillion,
+            totalFunding;
+
         for(i = 0; i < length; i++) {
-            dataset.push([data[i].longitude, data[i].latitude, data[i].total_money_raised, data[i].name, data[i].description]);
+            // total_money_raised attribute is currently in format of $6M or $316k
+            fundingString = data[i].total_money_raised;
+            fundingArray = fundingString.split("$");
+            thousandOrMillion = fundingArray[1][fundingArray[1].length - 1];
+
+            // formats funding number based on whether it's in millions or thousands
+            if (thousandOrMillion === "k") {
+                totalFunding = parseFloat(fundingArray[1]) / 1000;
+            } else if (thousandOrMillion === "M") {
+                totalFunding = parseFloat(fundingArray[1]);
+            }
+
+            // populates global variable dataset array
+            dataset.push([data[i].longitude, data[i].latitude, totalFunding, data[i].name, data[i].description]);
         }
-    }
+    },
+
 };
 
 $(document).ready(function() {
@@ -37,3 +56,78 @@ $(document).ready(function() {
 // var dataset = [
 //               [-73.989418, 40.739300, 14.3, "General Assembly", "info"], [-74.008068, 40.739395, 46, "Warby Parker", "info"]
 //               ];
+
+// var w = 1000;
+// var h = 1000;
+
+// var color = d3.scale.category20c();
+
+// var projection = d3.geo.mercator() 
+//   .center([-73.877220, 40.687635]) 
+//   .scale(255000) 
+//   .translate([752.2, 687.5]);
+
+// var svg = d3.select("body")
+//   .append("svg")
+//   .attr("width", w)
+//   .attr("height", h);
+
+// svg.selectAll("dot")
+//   .data(dataset)
+//   .enter()
+//   .append("circle")
+//   .attr("class", "dot")
+//   .attr("transform", function(d){
+//     return "translate(" + projection([d[0], d[1]]) + ")";
+//     })
+//   .attr("r", function(d) {
+//     return (d[2]/4);
+//   })
+//   .style("fill", function(d) {
+//     return color(d[2]);
+// });
+
+
+// var div = d3.select("body").append("div")
+//   .style("position", "absolute")
+//   .style("text-align", "left")
+//   .style("background", "white")
+//   .style("border-radius", "4px")
+//   .style("padding", "6px")
+//   .style("border", "solid 1px #DBDCDE")
+//   .style("opacity", 0);
+
+// function mouseover(d) {
+//   d3.select(this)
+//     .transition()
+//     .ease("elastic")
+//     .attr("r", (d[2]/2))
+//     .duration(500);
+
+//   div.html(d[3] + "<br />" + d[4])
+//     .style("left", (d3.event.pageX + 9) + "px")
+//     .style("top", (d3.event.pageY - 43) + "px")
+//     div
+//     .transition()
+//     .style("visibility", "visible")
+//     .style("opacity", 1)
+//     .duration(500);
+// }
+
+// function mouseout(d) {
+//   d3.select(this)
+//     .transition()
+//     .ease("elastic")
+//     .attr("r", (d[2]/4))
+//     .duration(1000);
+//   div
+//     .transition()
+//     .style("opacity", 1e-6)
+//     .style("visibility", "hidden")
+//     .duration(1000);
+
+// }
+
+// d3.selectAll(".dot")
+//   .on("mouseover", mouseover)
+//   .on("mouseout",  mouseout);
