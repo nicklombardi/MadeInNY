05826bca1c0cf1b14207ab028fe7162f8997eb76
nicklombardi <nicklombardi@gmail.com>
2013-09-04 18:23:20 -0400

    merging

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module MadeInNY
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-     # Enable the asset pipeline
+    config.assets.initialize_on_precompile = false
+
+    # Enable the asset pipeline
     config.assets.enabled = true
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ MadeInNY::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
