8625b7a17126bbbde2bbf76ed2c89d9324049d6f
Christine Coulter <christinetcoulter@gmail.com>
2013-09-05 16:21:53 -0400

    Finish writing tests for strong parameters in home controller. Questionable.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -16,4 +16,23 @@ describe HomeController do
       assigns(:silicons).should eq(silicons)
     end
   end
+
+  permitted_company_attr = [:name, :homepage_url, :category_code, :crunchbase_url, :twitter_username, :description, :founded_year, :total_money_raised, :address1, :address2, :city, :state_code, :zip_code, :latitude, :longitude, :number_of_employees]
+  permitted_for_company = permitted_company_attr.join(', ')
+
+  it "should permit: #{permitted_for_company}" do
+    controller.params = { company: permitted_company_attr }
+    controller.params.require(:company).should_receive(:permit).with(*permitted_company_attr)
+    controller.send :company_params
+  end
+
+  permitted_silicon_attr = [:name, :category_code]
+  permitted_for_silicon = permitted_silicon_attr.join(', ')
+
+  it "should permit: #{permitted_for_silicon}" do
+    controller.params = { silicon: permitted_silicon_attr }
+    controller.params.require(:silicon).should_receive(:permit).with(*permitted_silicon_attr)
+    controller.send :silicon_params
+  end
+
 end
