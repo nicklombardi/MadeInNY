2d699665c9f0c8a0edb31d71b2d0ec2f578a4001
nicklombardi <nicklombardi@gmail.com>
2013-09-12 12:56:33 -0400

    added cog for mouseover tooltip for all sections

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,13 +3,32 @@
 
 $(document).ready(function() {
 
-
   $(".glyphicon-cog").mouseenter(function(){
     console.log('mouseenter');
     $(".map-info").removeClass('hidden');
   });
 
   $(".glyphicon-cog").mouseleave(function(){
+    console.log('mouseout');
+  $(".map-info").addClass('hidden');
+  });
+
+  $('.data').find(".glyphicon-cog").mouseenter(function(){
+    console.log('mouseenter');
+    $(".map-info").removeClass('hidden');
+  });
+
+  $('.data').find(".glyphicon-cog").mouseleave(function(){
+    console.log('mouseout');
+  $(".map-info").addClass('hidden');
+  });
+
+  $("#key").find(".glyphicon").mouseenter(function(){
+    console.log('mouseenter');
+    $(".map-info").removeClass('hidden');
+  });
+
+  $("#key").find(".glyphicon").mouseleave(function(){
     console.log('mouseout');
   $(".map-info").addClass('hidden');
   });
